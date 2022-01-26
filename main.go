1a1a02b7cf7808b2f4b54ba0b6b6bc962645238d
Rishita Shaw <rishitashaw49@gmail.com>
2022-01-27 01:09:12 +0530

    first commit

---
+++ main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+	"time"
+	"math/rand"
+	"net/url"
+	"github.com/PuerkitoBio/goquery"
+)
+
+bringDomains := map[string]string{
+	"com":""
+}
+
+type SearchResult struct {
+	ResultRank int
+	ResultURL string
+	ResultTitle string
+	ResultDesc string
+}
+
+var userAgents = []string{
+	"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36",
+	"Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36",
+	"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36",
+	"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Safari/604.1.38",
+	"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:56.0) Gecko/20100101 Firefox/56.0",
+	"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Safari/604.1.38",
+
+}
+
+func randomUserAgent() string {
+
+}
+
+func buildBingURLs() string {
+
+}
+
+func scrapeClientRequest{
+
+}
+
+func BingScrape{
+
+}
+
+func bingResultParser{
+
+}
+
+func main() {
+	
+}
