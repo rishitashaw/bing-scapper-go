796c87f814fbd6c3596a19ce0fcaf8776c712845
Rishita Shaw <rishitashaw49@gmail.com>
2022-04-11 19:06:38 +0530

    bing domains added

--- main.go
+++ main.go
@@ -2,16 +2,51 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"net/http"
+	"net/url"
 	"strings"
 	"time"
-	"math/rand"
-	"net/url"
-	"github.com/PuerkitoBio/goquery"
 )
 
-bingDomains := map[string]string{
-	"com":""
+var bingDomains = map[string]string{
+	"com": "",
+	"uk": "&cc=GB",
+	"us": "&cc=US",
+	"tr": "&cc=TR",
+	"tw": "&cc=TW",
+	"ch": "&cc=CH",
+	"se": "&cc=SE",
+	"es": "&cc=ES",
+	"za": "&cc=ZA",
+	"sa": "&cc=SA",
+	"ru": "&cc=RU",
+	"ph": "&cc=PH",
+	"pt": "&cc=PT",
+	"pl": "&cc=PL",
+	"cn": "&cc=CN",
+	"no": "&cc=NO",
+	"nz": "&cc=NZ",
+	"nl": "&cc=NL",
+	"mx": "&cc=MX",
+	"my": "&cc=MY",
+	"kr": "&cc=KR",
+	"jp": "&cc=JP",
+	"it": "&cc=IT",
+	"id": "&cc=ID",
+	"in": "&cc=IN",
+	"hk": "&cc=HK",
+	"de": "&cc=DE",
+	"fr": "&cc=FR",
+	"fi": "&cc=FI",
+	"dk": "&cc=DK",
+	"cl": "&cc=CL",
+	"ca": "&cc=CA",
+	"br": "&cc=BR",
+	"be": "&cc=BE",
+	"at": "&cc=AT",
+	"au": "&cc=AU",
+	"ar": "&cc=AR",
 }
 
 type SearchResult struct {
@@ -113,7 +148,8 @@ func BingScrape(searchTerm string, proxyString interface{}, country string, page
 	return results, nil
 }
 
-func bingResultParser(){
+func bingResultParser(response *http.Response, rank int)([]SearchResult, error){
+
 
 }
 
